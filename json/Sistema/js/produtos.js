4bda3a942e92cbe8621963a0a0428e60ff8ff0a9
criaative <criaative.arte@gmail.com>
2017-03-07 11:49:44 -0300

    atualizar

--- json/Sistema/js/produtos.js
+++ json/Sistema/js/produtos.js
@@ -9,11 +9,11 @@ function carregaRegistros()
     $.getJSON('model/lista_produto.php', function (retorno) {
 
         $('#lista-produtos tbody').empty();
-
         retorno.forEach(function (obj, idx) {
 
+         
             var tr = '<tr obj-id="' + obj.id + '">'
-                    + '<td>' + obj.status + '</td>'
+                    + '<td id="status">' + obj.status + '</td>'
                     + '<td><a href="etiqueta.php?id=' + obj.id + '"><img src="http://localhost/CasaDosBanners/image/' + obj.image + '" width="90%"></a></td>'
                     + '<td>' + obj.sku + '</td>'
                     + '<td><a href="show_produto.php?id=' + obj.id + '">' + obj.produto + '</a></td>'
@@ -28,9 +28,14 @@ function carregaRegistros()
                     + '</a></td>'
                     + '</tr>';
             $('#lista-produtos tbody').append(tr);
-
         });
 
-    });
+
+    }
+    );
+
+
 
 }
+
+
