b4d0fc7e440b707885d9175f12e30127a25489fe
criaative <criaative.arte@gmail.com>
2017-03-08 13:33:06 -0300

    servidor instalado

--- config.php
+++ config.php
@@ -20,9 +20,9 @@ define('DIR_UPLOAD', 'C:/xampp/htdocs/CasaDosBanners/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
-define('DB_HOSTNAME', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_DATABASE', 'loja');
+define('DB_HOSTNAME', '179.188.16.205');
+define('DB_USERNAME', 'sistemacdb');
+define('DB_PASSWORD', 'C@s@123456');
+define('DB_DATABASE', 'sistemacdb');
 define('DB_PORT', '3306');
 define('DB_PREFIX', 'oc_');
